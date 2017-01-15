857689206c1a05228bb52b1f8914df4234bb3412
Salvatore Laisa <moebiusmania@gmail.com>
2017-01-15 23:06:07 +0100

    static routing, some plugin

--- hello/index.js
+++ hello/index.js
@@ -1,36 +1,85 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Good = require('good');
+const inert = require('inert');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
+
 server.connection({
     host: 'localhost',
-    port: 8000
+    port: 8080
 });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello',
-    handler: (request, reply) => {
+const goodSetup = {
+  register: Good,
+  options: {
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          response: '*',
+          log: '*'
+        }]
+      }, {
+          module: 'good-console'
+      }, 'stdout']
+    }
+  }
+}
+
+
+// Using inert plugin for static file serving
+server.register([inert, goodSetup], (err) => {
+  if (err) {
+      throw err;
+  }
+
+  // Add the route
+  server.route({
+      method: 'GET',
+      path:'/hello',
+      handler: (request, reply) => {
         return reply('hello world');
-    }
-});
+      }
+  });
 
-// Route with parameter
-server.route({
-    method: 'GET',
-    path: '/name/{name}',
-    handler: (request, reply) => {
+  // Route with parameter
+  server.route({
+      method: 'GET',
+      path: '/name/{name}',
+      handler: (request, reply) => {
+        server.log('info', request.params);
         reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
+      }
+  });
 
-// Start the server
-server.start((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+  server.route({
+      method: 'GET',
+      path: '/static',
+      handler: (request, reply) => {
+          reply.file('./hello/index.html');
+      }
+  });
+
+  server.route({
+      method: 'GET',
+      path: '/dir/{name*}',
+      handler: {
+        directory: {
+            path: './hello',
+            listing: true
+        }
+      }
+  });
+
+  // Start the server
+  server.start((err) => {
+      if (err) {
+          throw err;
+      }
+      server.log('info', `Server running at ${server.info.uri}`);
+  });
 });
